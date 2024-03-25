f09b6d07a709614eeece630f4cbd5c91662c5f53
KiarraB <kiarrabartlett@gmail.com>
2024-03-24 21:45:48 -0500

    It's a start - haven't completed front end yet.

---
+++ Java/src/main/java/LaunchCode/project/service/UserService.java
@@ -0,0 +1,10 @@
+package LaunchCode.project.service;
+
+import LaunchCode.project.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    User createUser(User user);
+    List<User> getAllUsers();
+}
