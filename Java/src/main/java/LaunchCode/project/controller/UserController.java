c9728a61e968471e5e688953b2fd0c482795f4ca
KiarraB <kiarrabartlett@gmail.com>
2024-04-18 18:15:58 -0500

    User, UserServiceImpl, JwtService, & Role Completed, UserService deleted & added into User Class, Unnecessary classes commented out

--- Java/src/main/java/LaunchCode/project/controller/UserController.java
+++ Java/src/main/java/LaunchCode/project/controller/UserController.java
@@ -1,37 +1,35 @@
 package LaunchCode.project.controller;
+//
+//import LaunchCode.project.models.User;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
 
-import LaunchCode.project.models.User;
-import LaunchCode.project.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/user")
-@CrossOrigin
-public class UserController {
-    @Autowired
-    private UserService userService;
-
-//    @GetMapping("/register")
-//    public String displayRegistration() {
-//        return "registration_form";
+//@RestController
+//@RequestMapping("/user")
+//@CrossOrigin
+//public class UserController {
+////    @Autowired
+////    private UserService userService;
+//
+////    @GetMapping("/register")
+////    public String displayRegistration() {
+////        return "registration_form";
+////    }
+//
+//    @PostMapping("/register_processing")
+//    public String registerProcessing(@RequestBody User user) {
+//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        String encodedPassword = passwordEncoder.encode(user.getPassword());
+//        user.setPassword(encodedPassword);
+//
+//        userService.createUser(user);
+//
+//        return "register_successful";
 //    }
-
-    @PostMapping("/register_processing")
-    public String registerProcessing(@RequestBody User user) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
-
-        userService.createUser(user);
-
-        return "register_successful";
-    }
-    @GetMapping("/users")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
-    }
-}
+//    @GetMapping("/users")
+//    public List<User> getAllUsers() {
+//        return userService.getAllUsers();
+//    }
+//}
--- Java/src/main/java/LaunchCode/project/service/UserServiceImpl.java
+++ Java/src/main/java/LaunchCode/project/service/UserServiceImpl.java
@@ -1,23 +1,22 @@
 package LaunchCode.project.service;
 
-import LaunchCode.project.models.User;
 import LaunchCode.project.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class UserServiceImpl implements UserService {
-    @Autowired
-    private UserRepository userRepository;
-    @Override
-    public User createUser(User user) {
-        return userRepository.save(user);
+public class UserServiceImpl implements UserDetailsService {
+    private final UserRepository repository;
+
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
     }
 
     @Override
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
